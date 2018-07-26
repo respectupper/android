d9166a0c7598a1000ac515ea40862a039a36b535
Respectupper <944174864@qq.com>
2018-07-26 19:03:51 +0800

    写剧本一级雏形

--- app/src/main/java/com/hhtxproject/piafriendscollege/NavFragment/WriteScript/fragment/SimpleFragment.java
+++ app/src/main/java/com/hhtxproject/piafriendscollege/NavFragment/WriteScript/fragment/SimpleFragment.java
@@ -4,23 +4,14 @@ package com.hhtxproject.piafriendscollege.NavFragment.WriteScript.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.hhtxproject.piafriendscollege.Entity.event.JumpEvent;
-import com.hhtxproject.piafriendscollege.Entity.event.SimpleDataEvent;
 import com.hhtxproject.piafriendscollege.R;
 import com.hhtxproject.piafriendscollege.Rx.RxBus;
 
@@ -28,20 +19,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-import static android.content.Context.INPUT_METHOD_SERVICE;
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SimpleFragment extends Fragment {
 
+    Unbinder unbinder;
+    @BindView(R.id.image)
+    ImageView image;
     @BindView(R.id.spinner)
     Spinner spinner;
-    Unbinder unbinder;
-    @BindView(R.id.toolbar_title)
-    TextView toolbarTitle;
-    @BindView(R.id.toolbar_title_right)
-    TextView toolbarTitleRight;
 
     public SimpleFragment() {
         // Required empty public constructor
@@ -61,8 +48,6 @@ public class SimpleFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_simple, container, false);
         unbinder = ButterKnife.bind(this, view);
-        setToolber();
-        setJump();
         return view;
     }
 
@@ -82,38 +67,41 @@ public class SimpleFragment extends Fragment {
         unbinder.unbind();
     }
 
-    private void setToolber(){
-        toolbarTitle.setText("返回");
-        toolbarTitleRight.setText("下一步");
-        toolbarTitle.setVisibility(View.VISIBLE);
-        toolbarTitleRight.setVisibility(View.VISIBLE);
-    }
+//    private void setToolber() {
+//        toolbarTitle.setText("返回");
+//        toolbarTitleRight.setText("下一步");
+//        toolbarTitle.setVisibility(View.VISIBLE);
+//        toolbarTitleRight.setVisibility(View.VISIBLE);
+//    }
 
-    private void setJump(){
-        toolbarTitle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RxBus.getDefault().post(new JumpEvent(-1));
-            }
-        });
+//    private void setJump() {
+//        toolbarTitle.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                RxBus.getDefault().post(new JumpEvent(-1));
+//            }
+//        });
+//
+//        toolbarTitleRight.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                if (TextUtils.isEmpty(aname)){
+////                    Toast.makeText(getContext(),"剧本名不能为空",Toast.LENGTH_SHORT).show();
+////                }else if (TextUtils.isEmpty(aclasss)){
+////                    Toast.makeText(getContext(),"剧本种类不能为空",Toast.LENGTH_SHORT).show();
+////                }else if (TextUtils.isEmpty(atitles)){
+////                    Toast.makeText(getContext(),"剧本标题不能为空",Toast.LENGTH_SHORT).show();
+////                }else if (TextUtils.isEmpty(aintroduce)){
+////                    Toast.makeText(getContext(),"剧本简介不能为空",Toast.LENGTH_SHORT).show();
+////                }else {
+////                    RxBus.getDefault().post(new JumpEvent(0));
+////                    SimpleDataEvent event = new SimpleDataEvent();
+////                    RxBus.getDefault().post(event);
+////                }
+//            }
+//        });
+//    }
+    private void setSpinner(){
 
-        toolbarTitleRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                if (TextUtils.isEmpty(aname)){
-//                    Toast.makeText(getContext(),"剧本名不能为空",Toast.LENGTH_SHORT).show();
-//                }else if (TextUtils.isEmpty(aclasss)){
-//                    Toast.makeText(getContext(),"剧本种类不能为空",Toast.LENGTH_SHORT).show();
-//                }else if (TextUtils.isEmpty(atitles)){
-//                    Toast.makeText(getContext(),"剧本标题不能为空",Toast.LENGTH_SHORT).show();
-//                }else if (TextUtils.isEmpty(aintroduce)){
-//                    Toast.makeText(getContext(),"剧本简介不能为空",Toast.LENGTH_SHORT).show();
-//                }else {
-//                    RxBus.getDefault().post(new JumpEvent(0));
-//                    SimpleDataEvent event = new SimpleDataEvent();
-//                    RxBus.getDefault().post(event);
-//                }
-            }
-        });
     }
 }
