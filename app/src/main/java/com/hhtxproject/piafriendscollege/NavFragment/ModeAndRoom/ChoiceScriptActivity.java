3a639c1d6657dd53efcc2a6008ef3761eda37cc2
HANHANGTXxuan <907160857@qq.com>
2018-08-07 13:49:03 +0800

    完善选择剧本

--- app/src/main/java/com/hhtxproject/piafriendscollege/NavFragment/ModeAndRoom/ChoiceScriptActivity.java
+++ app/src/main/java/com/hhtxproject/piafriendscollege/NavFragment/ModeAndRoom/ChoiceScriptActivity.java
@@ -1,11 +1,13 @@
 package com.hhtxproject.piafriendscollege.NavFragment.ModeAndRoom;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,7 +16,10 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.hhtxproject.piafriendscollege.Adapter.CSScriptAdapter;
 import com.hhtxproject.piafriendscollege.Adapter.ChoiceScriptAdapter;
@@ -31,12 +36,30 @@ public class ChoiceScriptActivity extends AppCompatActivity {
     Spinner spinner;
     @BindView(R.id.choice_script_up)
     ImageView choiceScriptUp;
-    @BindView(R.id.mRecyclerView)
-    RecyclerView mRecyclerView;
+    //    @BindView(R.id.mRecyclerView)
+//    RecyclerView mRecyclerView;
     @BindView(R.id.choice_script_down)
     ImageView choiceScriptDown;
     @BindView(R.id.mRecyclerViewTwo)
     RecyclerView mRecyclerViewTwo;
+    @BindView(R.id.radio1)
+    RadioButton radio1;
+    @BindView(R.id.radio2)
+    RadioButton radio2;
+    @BindView(R.id.radio3)
+    RadioButton radio3;
+    @BindView(R.id.radio4)
+    RadioButton radio4;
+    @BindView(R.id.radio5)
+    RadioButton radio5;
+    @BindView(R.id.radio6)
+    RadioButton radio6;
+    @BindView(R.id.radio7)
+    RadioButton radio7;
+    @BindView(R.id.radioGroup)
+    RadioGroup radioGroup;
+    @BindView(R.id.mSwipeRefreshLayout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     private ChoiceScriptAdapter mAdapter;
 
@@ -46,9 +69,98 @@ public class ChoiceScriptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choice_script);
         ButterKnife.bind(this);
         fullScreen(ChoiceScriptActivity.this);
-        addAdapter();
+//        addAdapter();
         addScriptAdapter();
+        mRadioButtonImgSize();
+        //设置RadioGroup的监听
+        initListener();
+        addSwipeRefreshLayout();
+    }
 
+    private void addSwipeRefreshLayout() {
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                switch (radioGroup.getCheckedRadioButtonId()){
+                    case R.id.radio1:
+                        Toast.makeText(ChoiceScriptActivity.this,"已经刷新", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio2:
+                        break;
+                    case R.id.radio3:
+                        break;
+                    case R.id.radio4:
+                        break;
+                    case R.id.radio5:
+                        break;
+                    case R.id.radio6:
+                        break;
+                    case R.id.radio7:
+                        break;
+                }
+//                2131296441
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void initListener() {
+        radioGroup.check(R.id.radio1);
+//        Toast.makeText(ChoiceScriptActivity.this, radioGroup.getChildAt(1) + "", Toast.LENGTH_SHORT).show();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.radio1:
+                        Toast.makeText(ChoiceScriptActivity.this, i + "", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio2:
+                        Toast.makeText(ChoiceScriptActivity.this, i + "", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio3:
+                        Toast.makeText(ChoiceScriptActivity.this, i + "", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio4:
+                        Toast.makeText(ChoiceScriptActivity.this, i + "", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio5:
+                        Toast.makeText(ChoiceScriptActivity.this, i + "", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio6:
+                        Toast.makeText(ChoiceScriptActivity.this, i + "", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio7:
+                        Toast.makeText(ChoiceScriptActivity.this, i + "", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void mRadioButtonImgSize() {
+        int q = dip2px(this, 60);
+        //定义底部标签图片大小
+        Drawable drawableFirst = getResources().getDrawable(R.drawable.radiobutton1);
+        drawableFirst.setBounds(0, 0, q, q);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        radio1.setCompoundDrawables(null, drawableFirst, null, null);//只放上面
+        Drawable drawableFirst1 = getResources().getDrawable(R.drawable.radiobutton2);
+        drawableFirst1.setBounds(0, 0, q, q);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        radio2.setCompoundDrawables(null, drawableFirst1, null, null);
+        Drawable drawableFirst2 = getResources().getDrawable(R.drawable.radiobutton1);
+        drawableFirst2.setBounds(0, 0, q, q);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        radio3.setCompoundDrawables(null, drawableFirst2, null, null);
+        Drawable drawableFirst3 = getResources().getDrawable(R.drawable.radiobutton1);
+        drawableFirst3.setBounds(0, 0, q, q);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        radio4.setCompoundDrawables(null, drawableFirst3, null, null);
+        Drawable drawableFirst4 = getResources().getDrawable(R.drawable.radiobutton1);
+        drawableFirst4.setBounds(0, 0, q, q);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        radio5.setCompoundDrawables(null, drawableFirst4, null, null);
+        Drawable drawableFirst5 = getResources().getDrawable(R.drawable.radiobutton1);
+        drawableFirst5.setBounds(0, 0, q, q);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        radio6.setCompoundDrawables(null, drawableFirst5, null, null);
+        Drawable drawableFirst6 = getResources().getDrawable(R.drawable.radiobutton1);
+        drawableFirst6.setBounds(0, 0, q, q);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        radio7.setCompoundDrawables(null, drawableFirst6, null, null);
     }
 
     private void addScriptAdapter() {
@@ -58,13 +170,25 @@ public class ChoiceScriptActivity extends AppCompatActivity {
         mRecyclerViewTwo.setAdapter(csScriptAdapter);
     }
 
-    private void addAdapter() {
-        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-//        横向
-//        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mRecyclerView.setLayoutManager(linearLayoutManager);
-        mAdapter = new ChoiceScriptAdapter(this);
-        mRecyclerView.setAdapter(mAdapter);
+//    private void addAdapter() {
+//        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+////        横向
+////        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        mRecyclerView.setLayoutManager(linearLayoutManager);
+//        mAdapter = new ChoiceScriptAdapter(this);
+//        mRecyclerView.setAdapter(mAdapter);
+//    }
+
+    /**
+     * 根据手机分辨率从DP转成PX
+     *
+     * @param context
+     * @param dpValue
+     * @return
+     */
+    public static int dip2px(Context context, float dpValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 
     /**
